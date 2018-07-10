32376e019cc734044364924cf92e8cdd2d5b21c7
LuisPalomo <luispalomosiv@gmail.com>
2018-07-10 04:05:43 +0200

    Added some styles

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { MapRoutingModule } from './map-routing.module';
 import { MapComponent } from './components/map/map.component';
+import { MatIconModule } from '../../../node_modules/@angular/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MapRoutingModule
-  ],
+  imports: [CommonModule, MapRoutingModule, MatIconModule],
   declarations: [MapComponent]
 })
-export class MapModule { }
+export class MapModule {}
