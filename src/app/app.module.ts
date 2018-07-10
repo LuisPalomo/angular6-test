5f812b3be7f6332ac289d7755fd7a270943733a2
LuisPalomo <luispalomosiv@gmail.com>
2018-07-10 02:43:22 +0200

    Added Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule, MatIconModule } from "@angular/material";
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatSidenavModule
+} from "@angular/material";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
