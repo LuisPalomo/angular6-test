3f9e281f52a1829a9a5f37bbb8d551f9e48c3b4a
Luis Manuel Palomo Sivianes <luismanuel.palomo@altran.es>
2018-07-24 23:27:21 +0200

    Added Angular Google Map dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,9 +8,21 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule, SharedModule],
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    SharedModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCQjN5pkNr7pkpGOLxFd9G5lULyttUI4NQ',
+    }),
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+  ]
 })
 export class AppModule {}
